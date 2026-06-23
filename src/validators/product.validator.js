0f61ebc19339512a9af1b1a73df088a5e96b3a96
Abhishek Shukla <abhishekshukla8097@gmail.com>
2026-06-23 12:23:12 +0530

    validation using zod

---
+++ src/validators/product.validator.js
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import { CATEGORIES } from "../constants/categories";
+
+export const getProductsQuerySchema = z.object({
+    limit: z.coerce.number().int().positive().max(100).optional(),
+    category: z
+        .string()
+        .trim()
+        .toLowerCase()
+        .refine((value) => CATEGORIES.includes(value), {
+            message: "Invalid Category"
+        })
+        .optional(),
+    cursor: z.string().optional(),
+    snapshot: z.string().datetime().optional()
+})
