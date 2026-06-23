983007fd053a688926fb5dcba8ebfbb4e83a4017
Abhishek Shukla <abhishekshukla8097@gmail.com>
2026-06-23 11:35:58 +0530

    Prisma configuration, utilities,

---
+++ src/constants/categories.js
@@ -0,0 +1,12 @@
+export const CATEGORIES = [
+    "electronics",
+    "books",
+    "clothing",
+    "beauty",
+    "sports",
+    "toys",
+    "grocery",
+    "furniture",
+    "home",
+    "automotive"
+]
