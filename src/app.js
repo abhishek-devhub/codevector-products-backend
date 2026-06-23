32b91a4f59fad05cf34f4a2dd819ded35a7753e3
Abhishek Shukla <abhishekshukla8097@gmail.com>
2026-06-23 20:49:03 +0530

    setup app and server entry point

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors"
+import helmet from "helmet";
+import morgan from "morgan";
+
+import { notFoundMiddleware } from "./middlewares/notFound.middleware.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
+import healthRoutes from "./routes/health.routes.js";
+import productRoutes from "./routes/product.routes.js";
+
+const app = express()
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/health', healthRoutes);
+app.use('/api/products', productRoutes);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+export default app;
